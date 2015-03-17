2461f53d9813237a2b860fed67d134b2dd195f36
Thomas King <thomas.king@de-cix.net>
2015-03-17 21:17:49 +0100

    Updated docu.

--- src/net/decix/alu/PhysicalPortOrLag.java
+++ src/net/decix/alu/PhysicalPortOrLag.java
@@ -1,6 +1,15 @@
 package net.decix.alu;
-
-/**
+/*
+ * This file is part of jFlowLib.
+ * 
+ * Copyright (c) 2009 DE-CIX Management GmbH <http://www.de-cix.net> - All rights
+ * reserved.
+ * 
+ * Author: Thomas King <thomas.king@de-cix.net>
+ * 
+ * This software is licensed under the Apache License, version 2.0. A copy of 
+ * the license agreement is included in this distribution.
+ * 
  * The XRS platform uses a new port mapping, starting in 11.0R4, as
  * follows:
  *
