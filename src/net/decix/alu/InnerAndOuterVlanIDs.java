f53edb7c07209478662d7192077bcd37ce620502
Thomas King <thomas.king@de-cix.net>
2015-03-17 21:18:23 +0100

    Updated docu

--- src/net/decix/alu/InnerAndOuterVlanIDs.java
+++ src/net/decix/alu/InnerAndOuterVlanIDs.java
@@ -1,5 +1,17 @@
 package net.decix.alu;
 
+/*
+ * This file is part of jFlowLib.
+ * 
+ * Copyright (c) 2009 DE-CIX Management GmbH <http://www.de-cix.net> - All rights
+ * reserved.
+ * 
+ * Author: Thomas King <thomas.king@de-cix.net>
+ * 
+ * This software is licensed under the Apache License, version 2.0. A copy of 
+ * the license agreement is included in this distribution.
+ */
+
 public class InnerAndOuterVlanIDs {
 	private int mInnerVlanId = 0;
 	private int mOuterVlanId = 0;
